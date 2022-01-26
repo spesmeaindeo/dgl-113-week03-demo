66d6d8cb60e64dc1ddd6ad0b390b0fada71763e4
nic-brian <86538642+nic-brian@users.noreply.github.com>
2022-01-25 22:47:08 -0800

    fixup html and automated tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -12,9 +12,33 @@ describe("index.html", () => {
     await page.type( '#description', 'eat')
     await page.click('#add');
 
+    await page.evaluate( () => document.getElementById("description").value = "")
+    await page.select('#duration', "50" );
+    await page.type( '#description', 'sleep')
+    await page.click('#add');
+
+    await page.evaluate( () => document.getElementById("description").value = "")
+    await page.select('#duration', "30" );
+    await page.type( '#description', 'study')
+    await page.click('#add');
+
     await page.waitForSelector('tbody');
     let element = await page.$('tbody');
-    let value = await page.evaluate(el => el.innerText, element);
-    expect(value).toEqual("hello");
+    let value = await page.evaluate(el => el.innerHTML, element);
+    expect(value).toEqual("<tr><th>1</th><td>20</td><td>eat</td></tr>" +
+                          "<tr><th>2</th><td>50</td><td>sleep</td></tr>" +
+                          "<tr><th>3</th><td>30</td><td>study</td></tr>");
+  });
+
+  it("clear todos", async () => {
+    await page.select('#duration', "20" );
+    await page.type( '#description', 'eat')
+    await page.click('#add');
+    await page.click('#clear');
+
+    await page.waitForSelector('tbody');
+    let element = await page.$('tbody');
+    let value = await page.evaluate(el => el.innerHTML, element);
+    expect(value).toEqual("");
   });
 });
